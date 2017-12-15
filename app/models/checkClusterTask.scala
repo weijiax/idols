13f9e4a2ddd9a61a39cf6e16c03f078ab97769b1
ruizhuhuang <ruizhuhuang@gmail.com>
2017-12-15 13:00:52 -0600

    format code nicely

--- app/models/checkClusterTask.scala
+++ app/models/checkClusterTask.scala
@@ -3,50 +3,44 @@ package models
 import play.api.mvc._
 import models.helperFunctions._
 
-
-class checkClusterTask(name: String, tType: String) extends Task(name, tType){
-   //name of this task, example: preprocessing, data analysis, postprocessing
+class checkClusterTask(name: String, tType: String) extends Task(name, tType) {
+  //name of this task, example: preprocessing, data analysis, postprocessing
   val taskName = name
   // type of this task, example: fileUpload
   val taskType = tType
-  
 
-   
-//  var file : File 
-//  var target : String 
-//  
-  def run(body: AnyContent) : String= {
+  //  var file : File 
+  //  var target : String 
+  //  
+  def run(body: AnyContent): String = {
     checkCluster(body)
   }
-  
-    /**
+
+  /**
    * check Cluster info, node list, etc.
    */
-  def checkCluster(body: AnyContent) : String = {
+  def checkCluster(body: AnyContent): String = {
     var feedback = ""
-    
-    val cluster  = new HadoopCluster(3,3,"Hadooop")
+
+    val cluster = new HadoopCluster(3, 3, "Hadooop")
     val reservationName = body.asFormUrlEncoded.get("reservation")(0)
-    
-    
-    
+
     try {
       val res_info = cluster.getReservationInfo(reservationName)
       val node_list = cluster.getHadoopClusterNodeList(reservationName)
       val break = "<p></p>"
       feedback = arrayToHtml("Reservation information:", res_info, "div") + break + arrayToHtml("Node list:", node_list, "div")
     } catch {
-      case e:Exception => {
+      case e: Exception => {
         feedback = "Failed: wrong reservation name"
       }
-    }   
-    
-    feedback
-    
     }
-  
+
+    feedback
+
+  }
+
   // description, 
   //tag is div, p, or others to form <div></div>
-  
-    
+
 }
--- app/models/runWordCountTask.scala
+++ app/models/runWordCountTask.scala
@@ -3,61 +3,56 @@ package models
 import play.api.mvc._
 import sys.process._
 
-
-class runWordCountTask(name: String, tType: String) extends Task(name, tType){
-   //name of this task, example: preprocessing, data analysis, postprocessing
+class runWordCountTask(name: String, tType: String) extends Task(name, tType) {
+  //name of this task, example: preprocessing, data analysis, postprocessing
   val taskName = name
   // type of this task, example: fileUpload
   val taskType = tType
-  
-   
-  
-//  var file : File 
-//  var target : String 
-//  
-  def run(body: AnyContent) : String= {
+
+  //  var file : File 
+  //  var target : String 
+  //  
+  def run(body: AnyContent): String = {
     runWordCount(body)
   }
-  
-    /**
+
+  /**
    * check Cluster info, node list, etc.
    */
-  def runWordCount(body: AnyContent) : String = {
+  def runWordCount(body: AnyContent): String = {
     var feedback = ""
-    
+
     val userInput = body.asFormUrlEncoded
     val script_path = userInput.get("script_path")(0)
-    
-    if (!new java.io.File(script_path).exists){
+
+    if (!new java.io.File(script_path).exists) {
       feedback = "Failed: script path does not exists. "
     }
-    
+
     val input_file_path = userInput.get("input_file_path")(0)
-    
-    if (!new java.io.File(input_file_path).exists){
-      if (!new java.io.File(script_path).exists){
+
+    if (!new java.io.File(input_file_path).exists) {
+      if (!new java.io.File(script_path).exists) {
         feedback = feedback + "Failed: input file path does not exists. "
       } else {
         feedback = "Failed: input file path does not exists. "
-      }      
+      }
     }
-    
-    
+
     val output_path = userInput.get("output_path")(0)
 
     val command = "source " + script_path + " " + input_file_path + " " + output_path + " &> /dev/null  & echo $!"
-    
-    if (new java.io.File(script_path).exists && new java.io.File(input_file_path).exists){
-      feedback = "Job submitted with process ID: " + Process(Seq("bash","-c", command)).!!.split("\n")(0)
+
+    if (new java.io.File(script_path).exists && new java.io.File(input_file_path).exists) {
+      feedback = "Job submitted with process ID: " + Process(Seq("bash", "-c", command)).!!.split("\n")(0)
     }
-    
-    
+
     feedback
-    
-//    res match {
-//      case 0 => "Job finished"
-//      case _ => "Something went wrong"
-//    }
+
+    //    res match {
+    //      case 0 => "Job finished"
+    //      case _ => "Something went wrong"
+    //    }
   }
-    
+
 }
